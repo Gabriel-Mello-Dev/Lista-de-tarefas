746e80a7f7cac95897ecc5907298975504cc9bc8
GabrielMello777 <gabrielmello8986@gmail.com>
2025-08-11 22:03:19 -0300

    melhorando aspecto visual

--- src/components/rodape/rodape.jsx
+++ src/components/rodape/rodape.jsx
@@ -3,7 +3,7 @@ import style from './rodape.module.css'
 const Rodape = ({ ano, criador }) => {
   return (
     <div className={style.rodape}>
-      <h3>React Não nativo-</h3>
+      <h3>Curso React-</h3>
       <h3>{ano}</h3>
       <h3>{criador}</h3>
     </div>
